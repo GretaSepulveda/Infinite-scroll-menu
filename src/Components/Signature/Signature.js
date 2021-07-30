8ac342a1e1687013267570eeb9d8d23e25bd879a
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-07-30 13:53:13 -0400

    fixed menu css

--- src/Components/Signature/Signature.js
+++ src/Components/Signature/Signature.js
@@ -3,18 +3,19 @@ import "./Signature.js";
 
 export default function Signature() {
   return (
-    <div className="Signature-container">
+    <div className="Signature-Container">
       <h3 className="Title">SIGNATURE</h3>
-      <div>
-        <h5>SALT FRIED CHICKEN $20</h5>
-        <p>Chicken/ olive oil/ salt</p>
+      <div className="Signature">
+        <div>
+          <h5>SALT FRIED CHICKEN $20</h5>
+          <p>Chicken/ olive oil/ salt</p>
 
-        <h5>CRAB WITH CURRY SOURCES $24.5</h5>
-        <p>Crab/ Potatoes/ Rice</p>
+          <h5>CRAB WITH CURRY SOURCES $24.5</h5>
+          <p>Crab/ Potatoes/ Rice</p>
 
-        <h5>IMPORTED SALMON STEAK $18.9</h5>
+          <h5>IMPORTED SALMON STEAK $18.9</h5>
           <p>Salmon/ Veggies/ Oil</p>
-        
+        </div>
       </div>
     </div>
   );
