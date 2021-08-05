5b1ca8c76878ecf0227378141dbf62b097874675
MariaGretaSepulveda <sepulveda.greta@gmail.com>
2021-08-05 11:57:48 -0500

    added in dumpling image

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Dumplingplate from "../Images/Dumplingplate.jpeg";
+
 
 export default function Carousel() {
   return (
     <div classname="Carousel-Container">
 
       <h2 className="Carousel-img">Carousel placeholder</h2>
-
+      <img src={Dumplingplate} alt=""/>
       
     
 
