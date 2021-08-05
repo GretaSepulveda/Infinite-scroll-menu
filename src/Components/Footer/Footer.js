72b14de81f132c99a3e382130348de117e78f2a7
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-08-05 12:57:34 -0400

    fixed footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,14 +1,13 @@
 import React from "react";
 import "./Footer.css";
 
-<div>
-  <p>© Copyright Grand Avenue</p>
-</div>;
-
 export default function Footer() {
   return (
-    <div>
-      <h2>Footer placeholdeR</h2>
+    <div ClassName="Footer-Container">
+      <div>
+        <p>© Copyright Grand Avenue</p>
+      </div>;
     </div>
   );
 }
+ 
