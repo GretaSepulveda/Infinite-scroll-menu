e311768815ae4e50ce0945a1a14c554a8f8a9974
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-08-08 20:48:19 -0400

    figured out font awesome

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,15 +1,15 @@
 import React from "react";
-import PotStickers from "../Images/PotStickers.jpeg";
 import "./About.css";
+import PotStickers from "../Images/PotStickers.jpeg";
 
 export default function About() {
   return (
-    <div classname="About-Container">
-      <div classname="About">
-        <h2>
+    <div classname="Story-Container">
+      <div classname="Our-Story">
+        <h3>
           Until I discovered our story I was never really interested in anything
-        </h2>
-        <h4>Our Founder John Phillipe</h4>
+        </h3>
+        <h5>Our Founder John Phillipe</h5>
       </div>
       <div className="PotStickers-container">
         <img src={PotStickers} alt="Pot stickers" />
---
+++ src/Components/FontAwesomeIcon/index.js
@@ -0,0 +1,4 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faStar, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faStar, faEnvelope);
