86b5ecea680c827516ff3dad31e116503895f3a3
MariaGretaSepulveda <sepulveda.greta@gmail.com>
2021-08-10 13:11:06 -0500

    updates onjs carousel component

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -4,28 +4,11 @@ import Dumplingplate from "../Images/Dumplingplate.jpeg";
 
 export default function Carousel() {
   return (
-
-    <div classname="Carousel-Container">
-
-<div id="background">
-  <h2 className="Carousel-img"></h2>
-      <img src={Dumplingplate} alt="Dumpling"/>
-      </div>
-      <div>
-        <h2>top carousel placeholder</h2>
-        <h2 className="Carousel-img">Top carousel img placeholder</h2>
-
-      </div>
-
-      <img src={Dumplingplate} alt="Dumpling" />
-
-
     <div className="Carousel-Container">
       <h4>Welcome to</h4>
       <h3>Grand Avenue</h3>
       <p>Making delicious premium food since 1950</p>
       {/* <img src={Dumplingplate} alt="Dumpling" /> */}
-
     </div>
   );
 }
