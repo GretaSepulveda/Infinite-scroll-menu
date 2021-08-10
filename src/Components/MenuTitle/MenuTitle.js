9bf44c3c79153935c8eac44ff7ce18c50f631217
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-08-10 14:01:16 -0400

    add 2 fonts and working on line spacing

--- src/Components/MenuTitle/MenuTitle.js
+++ src/Components/MenuTitle/MenuTitle.js
@@ -5,7 +5,7 @@ export default function MenuTitle() {
   return (
     <div>
       <div className="Menu-title-text">
-        <h3>Chef menu</h3>
+        <h3 id="Dancing-script">Chef menu</h3>
         <p>Check out our signature menus</p>
       </div>
     </div>
