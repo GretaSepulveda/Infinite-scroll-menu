5843c1b220b3e754cae79ad51870293c3fbc4842
MariaGretaSepulveda <sepulveda.greta@gmail.com>
2021-08-10 13:20:10 -0500

    updated css nav bar

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -1,14 +1,12 @@
 import React from "react";
 import "./Carousel.css";
-import Dumplingplate from "../Images/Dumplingplate.jpeg";
 
 export default function Carousel() {
   return (
     <div className="Carousel-Container">
-      <h4>Welcome to</h4>
-      <h3>Grand Avenue</h3>
+      <h4 id="Dancing-script">Welcome to</h4>
+      <h2>Grand Avenue</h2>
       <p>Making delicious premium food since 1950</p>
-      {/* <img src={Dumplingplate} alt="Dumpling" /> */}
     </div>
   );
 }
