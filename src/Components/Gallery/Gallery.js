2c470b13f8da8b9bb21e9268f33f70f91ea76729
MariaGretaSepulveda <sepulveda.greta@gmail.com>
2021-08-11 11:48:19 -0500

    updated gallery js

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -2,6 +2,37 @@ import React from "react";
 import Gallery from 'react-grid-gallery';
 
 
+const IMAGES =
+[{
+        src: "Burgergrilling.jpeg",
+        thumbnail: "Burgergriling.jpeg",
+        thumbnailWidth: 320,
+        thumbnailHeight: 174,
+        isSelected: true,
+        caption: "Burger time"
+},
+{
+        src: "Chefmenu.png",
+        thumbnail: "galleryimage.jpeg",
+        thumbnailWidth: 320,
+        thumbnailHeight: 212,
+        tags: [{value: "Ocean", title: "Ocean"}, {value: "People", title: "People"}],
+        caption: "Boats (Jeshu John - designerspics.com)"
+},
+
+{
+        src: "Pasta.jpeg",
+        thumbnail: "Pasta.jpeg",
+        thumbnailWidth: 320,
+        thumbnailHeight: 212
+}]
+
+render(
+        <Gallery images={IMAGES}/>,
+        document.getElementById('example-0')
+);
+
+
 
 export default function Gallery() {
   return (
