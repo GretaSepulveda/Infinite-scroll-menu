9737400d9bd0bf0d4e453b724e1078c0789b84e7
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-08-11 14:10:22 -0400

    more css

--- src/Components/MenuTitle/MenuTitle.js
+++ src/Components/MenuTitle/MenuTitle.js
@@ -3,9 +3,10 @@ import "./MenuTitle.css";
 
 export default function MenuTitle() {
   return (
-    <div>
+    <div className="Menu-title-container">
       <div className="Menu-title-text">
-        <h3 id="Dancing-script">Chef menu</h3>
+        <h3 id="Dancing-script">Chef</h3>
+        <h3>Menu</h3>
         <p>Check out our signature menus</p>
       </div>
     </div>
