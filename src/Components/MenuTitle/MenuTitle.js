70b1b40e337db12dc377c7521883e269662d4939
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-08-12 17:11:41 -0400

    new style choices

--- src/Components/MenuTitle/MenuTitle.js
+++ src/Components/MenuTitle/MenuTitle.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Parallax } from "react-parallax";
-import Burgergrilling from "../Images/Burgergrilling.jpeg";
+import Porktenderloin from "../Images/pork-tenderloin.jpg";
 import "./MenuTitle.css";
 
 export default function MenuTitle() {
   return (
     <div className="Menu-title-container">
-      <Parallax className="Parallax" bgImage={Burgergrilling}>
+      <Parallax className="Parallax" bgImage={Porktenderloin}>
         <div className="Menu-title-text">
           <h3 id="Dancing-script">Chef</h3>
           <h3>Menu</h3>
