015da3435b06ad1f2d142c0e40fbb2ad10359490
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-08-12 14:02:51 -0400

    added new photos and parallax

--- src/Components/Carousel/Carousel.js
+++ src/Components/Carousel/Carousel.js
@@ -1,16 +1,20 @@
 import React from "react";
+import { Parallax } from "react-parallax";
+import DumplingPlate from "../Images/Dumplingplate.jpeg";
 import "./Carousel.css";
 
 export default function Carousel() {
   return (
     <div className="Carousel-Container">
-      <div className="Welcome-Message">
-        <h4 id="Dancing-script">
-          <mark class="Specific-text">Welcome</mark> to
-        </h4>
-        <h2>Grand Avenue</h2>
-        <p>Making delicious premium food since 1950</p>
-      </div>
+      <Parallax className="Carousel" bgImage={DumplingPlate}>
+        <div className="Welcome-Message">
+          <h4 id="Dancing-script">
+            <mark class="Specific-text">Welcome</mark> to
+          </h4>
+          <h2>Grand Avenue</h2>
+          <p>Making delicious premium food since 1950</p>
+        </div>
+      </Parallax>
     </div>
   );
 }
