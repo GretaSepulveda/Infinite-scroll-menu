1d1d76d32e70b8b719cf85dd686b25cc691c4ba5
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-08-13 21:34:56 -0400

    added a loader

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import "./Components/FontAwesomeIcon";
+import React, { useState, useEffect } from "react";
+import BounceLoader from "react-spinners/BounceLoader";
 
+import "./Components/FontAwesomeIcon";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import Menu from "./Components/Menu/Menu";
@@ -9,14 +11,34 @@ import Footer from "./Components/Footer/Footer";
 import Gallery from "./Components/Gallery/Gallery";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <div className="App">
-      <Navbar />
-      <Carousel />
-      <About />
-      <Menu />
-      <Gallery />
-      <Footer />
+      {loading ? (
+        <BounceLoader
+          size={80}
+          color={"#aca358"}
+          loading={loading}
+          speedMultiplier={1.5}
+        />
+      ) : (
+        <div>
+          <Navbar />
+          <Carousel />
+          <About />
+          <Menu />
+          <Gallery />
+          <Footer />
+        </div>
+      )}
     </div>
   );
 }
