e4c519ee00a99f4f270bd9f1cc50d736da208309
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-08-17 13:16:01 -0400

    reconfigured the merged files

--- src/Components/Gallery/image-slider.component.js
+++ src/Components/Gallery/image-slider.component.js
@@ -1,56 +1,56 @@
 import React from "react";
-import "../node_modules/react-image-gallery/styles/css/image-gallery.css";
+// import "../node_modules/react-image-gallery/styles/css/image-gallery.css";
 import ImageGallery from "react-image-gallery";
 
 const images = [
-{
+  {
     original: "galleryimage.jpeg",
     thumbnail: "galleryimage.jpeg",
-},
-{
+  },
+  {
     original: "Burgergrilling.jpeg",
     thumbnail: "Burgergrilling.jpeg",
-},
-{
+  },
+  {
     original: "Pasta.jpeg",
     thumbnail: "Pasta.jpeg",
-},
+  },
 ];
 
 class ImageSliderComponent extends React.Component {
-constructor(props) {
+  constructor(props) {
     super(props);
     this.myImageRef = React.createRef();
     this.onFullscreen = this.onFullscreen.bind(this);
     this.onPlay = this.onPlay.bind(this);
     this.onPause = this.onPause.bind(this);
     this.onMoveToSlide = this.onMoveToSlide.bind(this);
-}
+  }
 
-onFullscreen() {
-    this.myImageRef.current.fullScreen()
-}
-onPlay() {
-    this.myImageRef.current.play()
-}
-onPause() {
-    this.myImageRef.current.pause()
-}
-onMoveToSlide() {
+  onFullscreen() {
+    this.myImageRef.current.fullScreen();
+  }
+  onPlay() {
+    this.myImageRef.current.play();
+  }
+  onPause() {
+    this.myImageRef.current.pause();
+  }
+  onMoveToSlide() {
     // Index start from 0 so 2 will move to 3rd slide
-    this.myImageRef.current.slideToIndex(2)
-}
+    this.myImageRef.current.slideToIndex(2);
+  }
 
-render() {
+  render() {
     return (
-    <div className="wrapper">
-        <ImageGallery items={images}  ref={this.myImageRef} />
+      <div className="wrapper">
+        <ImageGallery items={images} ref={this.myImageRef} />
         <button onClick={this.onFullscreen}>FullScreen</button>
         <button onClick={this.onPlay}>Play</button>
         <button onClick={this.onPause}>Pause</button>
         <button onClick={this.onMoveToSlide}>SlideTo 3rd Slide</button>
-    </div>
+      </div>
     );
-}
+  }
 }
 export default ImageSliderComponent;
