360061354cf5d81177ba1e92cc072891a6dca26f
Dwaylan <Dwaylan.Applewhite@gmail.com>
2021-08-18 12:29:56 -0400

    working with transform3d css

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { GoogleMap, Marker, withGoogleMap } from "react-google-maps";
+import {
+  GoogleMap,
+  Marker,
+  withGoogleMap,
+  withScriptjs,
+} from "react-google-maps";
 import "./Map.css";
 
 // const WrappedMap = withScriptjs(withGoogleMap(Map));
@@ -19,10 +24,26 @@ import "./Map.css";
 //   );
 // }
 
+const MapComponent = withScriptjs(
+  withGoogleMap((props) => (
+    <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+      {props.isMarkerShown && (
+        <Marker position={{ lat: -32.684891, lng: -97.052597 }} />
+      )}
+    </GoogleMap>
+  ))
+);
+
 export default function Map() {
   return (
     <div className="Map-Parent-Container">
-      <h2>Map here</h2>
+      <MapComponent
+        isMarkerShown
+        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAkoZwyufnxZlTYZPX1gM00r7fjIPW3oYM"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `400px` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
     </div>
   );
 }
