84f0d83bad437fc9e7deb5ed7a9f838409378060
MariaGretaSepulveda <sepulveda.greta@gmail.com>
2021-08-25 11:56:32 -0500

    updated footer removed extra sm

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -1,34 +1,4 @@
 import React from "react";
-import Imgix from "react-imgix";
-import Burgergrilling from "Images";
-
-
-const images = [
-  "",
-  
-  
-];
-
-
-
-export const Gallery = () => (
-  <div className="gallery">
-    {images.map(image => (
-      <Imgix
-        sizes="(min-width: 960px) 33vw, (min-width: 640px) 50vw, 100vw"
-        src={buildURL(image)}
-        key={image}
-        imgixParams={{
-          fit: "crop",
-          fm: "jpg"
-        }}
-        width="600"
-        height="600"
-      />
-    ))}
-  </div>
-);
-
 import Gallery from "react-grid-gallery";
 import "./Gallery.css";
 
@@ -65,25 +35,47 @@ const IMAGES = [
   },
   {
     src:
-      "https://cdn.pixabay.com/photo/2015/09/05/20/00/dim-sum-924912_960_720.jpg",
-    alt: "Dim sum",
+      "https://cdn.pixabay.com/photo/2016/08/05/07/15/pizza-1571697_960_720.jpg",
+    alt: "Baked potato pizza",
     thumbnail:
-      "https://cdn.pixabay.com/photo/2015/09/05/20/00/dim-sum-924912_960_720.jpg",
+      "https://cdn.pixabay.com/photo/2016/08/05/07/15/pizza-1571697_960_720.jpg",
     thumbnailWidth: 320,
     thumbnailHeight: 212,
-    isSelected: true,
-    caption: "Dim sum",
+    // isSelected: true,
+    caption: "Baked potato pizza",
   },
   {
     src:
-      "https://cdn.pixabay.com/photo/2015/09/05/20/00/dim-sum-924912_960_720.jpg",
-    alt: "Dim sum",
+      "https://cdn.pixabay.com/photo/2020/09/13/15/41/steak-5568608_960_720.jpg",
+    alt: "Grilled steak",
     thumbnail:
-      "https://cdn.pixabay.com/photo/2015/09/05/20/00/dim-sum-924912_960_720.jpg",
+      "https://cdn.pixabay.com/photo/2020/09/13/15/41/steak-5568608_960_720.jpg",
     thumbnailWidth: 320,
     thumbnailHeight: 212,
-    isSelected: true,
-    caption: "Dim sum",
+    // isSelected: true,
+    caption: "Grilled steak",
+  },
+  {
+    src:
+      "https://cdn.pixabay.com/photo/2015/10/12/15/25/bar-984527_960_720.jpg",
+    alt: "Restaurant",
+    thumbnail:
+      "https://cdn.pixabay.com/photo/2015/10/12/15/25/bar-984527_960_720.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 212,
+    // isSelected: true,
+    caption: "Restaurant",
+  },
+  {
+    src:
+      "https://cdn.pixabay.com/photo/2018/08/13/13/37/roasted-chicken-3602956_960_720.jpg",
+    alt: "Roasted chicken",
+    thumbnail:
+      "https://cdn.pixabay.com/photo/2018/08/13/13/37/roasted-chicken-3602956_960_720.jpg",
+    thumbnailWidth: 320,
+    thumbnailHeight: 212,
+    // isSelected: true,
+    caption: "Roasted chicken",
   },
 ];
 
@@ -102,4 +94,3 @@ export default function GridGallery() {
     </div>
   );
 }
-
